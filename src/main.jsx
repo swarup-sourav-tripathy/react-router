561905898685d4b5354b8d0d0ca2ad689d8aaa45
swarup <swaruptripathy03@gmail.com>
2026-06-20 17:03:28 +0530

    1st issue

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
-import User from './components/User/user.jsx'
+import User from './components/User/User.jsx'
 import { Route, RouterProvider , createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
 import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
